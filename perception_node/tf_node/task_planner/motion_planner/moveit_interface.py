fbea78ad32d1e7057bf4b7e1999a801891988095
Chandni <chandnikumarikumari1@gmail.com>
2026-02-10 12:39:51 +0530

    Create moveit_interface.py

---
+++ perception_node/tf_node/task_planner/motion_planner/moveit_interface.py
@@ -0,0 +1,16 @@
+class MoveItInterface:
+    def execute(self, action):
+        print(f"Executing motion: {action}")
+
+if __name__ == "__main__":
+    executor = MoveItInterface()
+    actions = [
+        "move_home",
+        "pre_grasp",
+        "approach",
+        "grasp",
+        "lift",
+        "place"
+    ]
+    for a in actions:
+        executor.execute(a)
